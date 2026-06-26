4c50454bff41c4f89044e950e9d89dc438ed5f6b
KirgizAzur <zarubin_vadik@mail.ru>
2026-06-26 23:51:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -170,8 +170,7 @@ fi
 cat > "$nginx_conf" <<EOF
 server {
     # Слушаем только локально
-    listen 127.0.0.1:${nginx_port} ssl;
-    http2 on; 
+    listen 127.0.0.1:${nginx_port} ssl http2; 
     server_name ${domain};
 
     # Ваши готовые сертификаты
